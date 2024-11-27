a425b817bfa58dbd81593cbcf7cb3934b6e91061
Desmond-Fon <desmondfon@yahoo.com>
2024-11-27 02:32:45 +0100

    completed desktop view

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,36 +1,28 @@
 const Slider = () => {
     return (
-        <>
-            <div className="overflow-hidden bg-white border-black border-[2px] py-3">
-                <div className="relative z-[0]">
-                    <div className=" flex justify-center relative z-[999]">
-                        <div className="-mx-10 flex justify-center sliding-text-box w-[110%]  z-[999] relative">
-                            <div className="sliding-text flex justify-center  bg-white font-sigmar items-center rotate-5 gap-[30px] font-fruktur">
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                <p className="text-[24px] text-black">$Bloob</p>
-                                {/* <p className="text-[24px] text-black">$Bloob</p>
-                <p className="text-[24px] text-black">$Bloob</p>
-                <p className="text-[24px] text-black">$Bloob</p>
-                <p className="text-[24px] text-black">$Bloob</p>
-                <p className="text-[24px] text-black">$Bloob</p>
-                <p className="text-[24px] text-black">$Bloob</p>
-                <p className="text-[24px] text-black">$Bloob</p>
-                <p className="text-[24px] text-black">$Bloob</p>
-                <p className="text-[24px] text-black">$Bloob</p> */}
-                            </div>
+        <div className="overflow-hidden bg-white border-black border-[2px] py-3">
+            <div className="relative z-[0]">
+                <div className="flex justify-center relative z-[999]">
+                    <div className="sliding-text-box w-[110%] z-[999] relative">
+                        <div className="sliding-text flex justify-center items-center font-sigmar gap-[30px]">
+                            {/* Original content */}
+                            <p className="text-[24px] text-black">$Bloob</p>
+                            <p className="text-[24px] text-black">$Bloob</p>
+                            <p className="text-[24px] text-black">$Bloob</p>
+                            <p className="text-[24px] text-black">$Bloob</p>
+                            <p className="text-[24px] text-black">$Bloob</p>
+
+                            {/* Duplicate content */}
+                            <p className="text-[24px] text-black">$Bloob</p>
+                            <p className="text-[24px] text-black">$Bloob</p>
+                            <p className="text-[24px] text-black">$Bloob</p>
+                            <p className="text-[24px] text-black">$Bloob</p>
+                            <p className="text-[24px] text-black">$Bloob</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
